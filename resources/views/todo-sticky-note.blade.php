62c8d475c82fd6dcd802ada036d1eb1ab0067bb0
Ahmet Barut <ahmetbarut588@gmail.com>
2023-02-24 02:21:28 +0300

    fixed migrations and asset publish

--- resources/views/todo-sticky-note.blade.php
+++ resources/views/todo-sticky-note.blade.php
@@ -9,7 +9,6 @@
         link.href = '/vendor/sticky-todos/sticky-todos.css';
         link.media = 'all';
         head.appendChild(link);
-        add js to body
         var body = document.getElementsByTagName('body')[0];
         var script = document.createElement('script');
         script.type = 'text/javascript';
